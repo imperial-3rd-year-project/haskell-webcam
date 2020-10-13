647a3e06ab179d462385de62175f34b32afa0ffd
Roko  Parac <roko.parac18@imperial.ac.uk>
2020-10-13 21:12:21 +0100

    Get devices

--- Main.hs
+++ Main.hs
@@ -1,4 +1,11 @@
 module Main where
 
+import Graphics.Capture.Class
+import Graphics.Capture.V4L2.Device
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  devs <- getDevices :: IO [Device U]
+  putStrLn $ show devs
+  putStrLn $ deviceDescription (Unopened "/dev/video0")
+  putStrLn "Hello, Haskell!"
--- src/Graphics/Capture/V4L2/Device.hs
+++ src/Graphics/Capture/V4L2/Device.hs
@@ -1,8 +1,12 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GADTs, StandaloneDeriving #-}
 
 module Graphics.Capture.V4L2.Device (Device (..)) where
 
+import Control.Monad (filterM)
+import Data.List (isPrefixOf)
 import Graphics.Capture.Class
+import System.Directory (listDirectory, pathIsSymbolicLink)
+import System.FilePath.Posix ((</>))
 import System.Posix.Internals (FD)
 
 data Device a where
@@ -10,8 +14,22 @@ data Device a where
   Opened    :: FD -> FilePath -> Device O
   Streaming :: FD -> FilePath -> Device S
 
+deriving instance Show (Device a)
 
 instance VideoCapture Device where
   deviceDescription (Unopened path)    = path
   deviceDescription (Opened _ path)    = path
   deviceDescription (Streaming _ path) = path
+    
+  -- finds all /dev/video* which are not links and wraps them in Device type
+  getDevices = do
+    devs <- listDirectory deviceDir
+
+    let relNameDevs  = filter (isPrefixOf videoDevPrefix) devs
+        fullNameDevs = map (deviceDir </>) relNameDevs
+
+    videoDevs <- filterM (fmap not . pathIsSymbolicLink) fullNameDevs
+    return $ map Unopened videoDevs
+    where
+      deviceDir       = "/dev"
+      videoDevPrefix  = "video"
