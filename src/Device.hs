5c8e60dc9530e111121e1cbb74f9bae0254c1e33
Roko  Parac <roko.parac18@imperial.ac.uk>
2020-10-13 14:41:19 +0100

    Nicer device declaration

--- src/Device.hs
+++ src/Device.hs
@@ -1,9 +1,20 @@
-module Device (Device (Device), deviceDescription ) where 
+{-# LANGUAGE GADTs #-}
 
--- Barebones declaration for now. To be extended when we see what data needs to be stored
-data Device = Device String
+module Device (Device (Unopened, Opened, Streaming), deviceDescription ) where 
+
+import System.Posix.Internals(FD)
+import Types
+
+data Device a where
+  Unopened  :: FilePath       -> Device U
+  Opened    :: FD -> FilePath -> Device O
+  Streaming :: FD -> FilePath -> Device S
 
 
-deviceDescription :: Device -> String
-deviceDescription (Device name) = name
+
+
+deviceDescription :: Device a -> String
+deviceDescription (Unopened path)    = path
+deviceDescription (Opened _ path)    = path
+deviceDescription (Streaming _ path) = path
 
