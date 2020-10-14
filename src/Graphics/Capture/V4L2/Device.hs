cf2fa33f231ef0f3331dfc92002bafcb02408e73
Roko Parac <rp218@ic.ac.uk>
2020-10-14 11:34:48 +0200

    Implements openDevice in Device.hs

--- src/Graphics/Capture/V4L2/Device.hs
+++ src/Graphics/Capture/V4L2/Device.hs
@@ -7,12 +7,13 @@ import Data.List (isPrefixOf)
 import Graphics.Capture.Class
 import System.Directory (listDirectory, pathIsSymbolicLink)
 import System.FilePath.Posix ((</>))
-import System.Posix.Types (Fd)
+
+import qualified Graphics.V4L2.Device as V4L2
 
 data Device a where
   Unopened  :: FilePath       -> Device U
-  Opened    :: Fd -> FilePath -> Device O
-  Streaming :: Fd -> FilePath -> Device S
+  Opened    :: V4L2.Device -> FilePath -> Device O
+  Streaming :: V4L2.Device -> FilePath -> Device S
 
 deriving instance Show (Device a)
 
@@ -34,4 +35,6 @@ instance VideoCapture Device where
       deviceDir       = "/dev"
       videoDevPrefix  = "video"
 
-
+  openDevice (Unopened path) = do
+    v4l2Dev <- V4L2.openDevice path
+    return $ Opened v4l2Dev path
